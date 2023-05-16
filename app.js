51e1f28bedc9fb6d57aa0b4ce759f2d6586744f9
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-05-16 14:36:03 +0100

    handled error and outside search routes

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Error handler
+// app.use((err, req, res, next) => {
+//     console.log(err);
+//     res.status(500).send("Something went wrong");
+// });
+
+app.use(function (err, req, res, next) {
+  res
+    .status(422)
+    .send({ error: err.message, message: "Invalid data provided" });
+}); // action could not be processed properly due to invalid data provided
+
+app.get("*", function (req, res) {
+  res.send("Sorry, this is an invalid URL.");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port: 3000");
 });
