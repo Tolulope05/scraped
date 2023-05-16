93855d577c786e650abab2be7d25f9a001ca4c71
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-05-16 14:32:51 +0100

    added important dependencies and middlewares

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const cheerio = require("cheerio");
 const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
